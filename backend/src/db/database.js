cc4dd6dd19980b8a35836544df1a3bc69ac50537
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2023-02-02 18:46:13 -0800

    changing the database connection string and options

--- backend/src/db/database.js
+++ backend/src/db/database.js
@@ -3,10 +3,19 @@ const logger = require("../logger");
 const DB_HOST = process.env.DB_HOST || "localhost";
 const DB_USER = process.env.DB_USER || "default";
 const DB_PWD = process.env.DB_PWD || "default";
+const DB_PORT = process.env.DB_PORT || 27017;
 const database = async () => {
   mongoose.set("strictQuery", false);
   try {
-    await mongoose.connect(`mongodb://${DB_USER}:${DB_PWD}@${DB_HOST}:27017/pub-code?authSource=admin`);
+    await mongoose.connect(`mongodb://${DB_USER}:${DB_PWD}@${DB_HOST}:${DB_PORT}/pub-code`,{
+      authSource: "admin",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      maxPoolSize: 20, // Maintain up to 10 socket connections
+      minPoolSize: 5, // Maintain at least 5 socket connections
+      serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+      socketTimeoutMS: 60000, // Close sockets after 45 seconds of inactivity
+    });
     mongoose.connection.on("disconnected", () => {
       logger.error("disconnected from mongodb");
     });
