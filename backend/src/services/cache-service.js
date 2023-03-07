5d5499454ab73d7fe81f00107fb5360c73455cb6
Om Mishra <32200996+mishraomp@users.noreply.github.com>
2023-03-07 10:33:43 -0800

    replace cache entries instead of infinitely adding items to it. (#144)

--- backend/src/services/cache-service.js
+++ backend/src/services/cache-service.js
@@ -1,23 +1,29 @@
 const pubcodeEntity = require("../entities/pub-code-entity");
-let pubCodes = [];
+let PUB_CODES = [];
 const cacheService = {
 
   async loadAllPubCodes() {
-      const data = await pubcodeEntity.find({});
-      for(const individualData of data){
-        const normalizedResult = {
-          repo_name: individualData.repo_name,
-          ...individualData.product_information,
-          ...individualData.product_technology_information,
-          ...individualData.product_external_dependencies,
-          ...individualData.data_management_roles
-        };
-        pubCodes.push(normalizedResult);
-      }
-    return pubCodes;
+    const data = await pubcodeEntity.find({});
+    const newPubCodes = [];
+    for (const individualData of data) {
+      const normalizedResult = {
+        repo_name: individualData.repo_name,
+        ...individualData.product_information,
+        ...individualData.product_technology_information,
+        ...individualData.product_external_dependencies,
+        ...individualData.data_management_roles
+      };
+      newPubCodes.push(normalizedResult);
+    }
+    // clear the existing cache
+    if (PUB_CODES && PUB_CODES.length > 0) {
+      PUB_CODES = [];
+    }
+    PUB_CODES = newPubCodes;
+    return PUB_CODES;
   },
   getAllPubCodes() {
-    return pubCodes;
+    return PUB_CODES;
   }
 };
 module.exports = cacheService;
