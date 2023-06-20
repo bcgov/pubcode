4927a6950c3df423d2f879a6ec22920fb35ad8a6
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2023-06-19 17:44:27 -0700

    fix file to support react 17.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 import App from "./App.jsx";
 import "./index.css";
-import ReactDOM from "react-dom/client";
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <App />
-);
+import ReactDOM from "react-dom";
+ReactDOM.render(<App />, document.getElementById('root'));
