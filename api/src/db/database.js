a252f48cc2451c220498c949faff7aad73b532b8
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2023-09-21 11:23:13 -0700

    retry db connection
    Signed-off-by: OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>

--- api/src/db/database.js
+++ api/src/db/database.js
@@ -5,19 +5,20 @@ const DB_USER = process.env.DB_USER || "default";
 const DB_PWD = process.env.DB_PWD || "default";
 const DB_PORT = process.env.DB_PORT || 27017;
 const DB_NAME = process.env.DB_NAME || "pubcode";
+const retry = require("async-retry");
 const database = async () => {
-  mongoose.set("strictQuery", false);
-  try {
-    const connectionUri =`mongodb://${DB_USER}:${DB_PWD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+  await retry(async () => {
+    mongoose.set("strictQuery", false);
+    const connectionUri = `mongodb://${DB_USER}:${DB_PWD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
     logger.info(`connecting to mongodb on url: ${connectionUri}`);
-    await mongoose.connect(connectionUri,{
+    await mongoose.connect(connectionUri, {
       authSource: "admin",
       useNewUrlParser: true,
       useUnifiedTopology: true,
       maxPoolSize: 20, // Maintain up to 10 socket connections
       minPoolSize: 5, // Maintain at least 5 socket connections
       serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-      socketTimeoutMS: 60000, // Close sockets after 45 seconds of inactivity
+      socketTimeoutMS: 60000 // Close sockets after 45 seconds of inactivity
     });
     mongoose.connection.on("disconnected", () => {
       logger.error("disconnected from mongodb");
@@ -25,11 +26,7 @@ const database = async () => {
     mongoose.connection.on("reconnected", () => {
       logger.info("reconnected to mongodb");
     });
-  } catch (error) {
-    logger.error(error);
-    if(error.stack?.includes("MongooseServerSelectionError")){
-      process.exit(2);// exit with error code 2 for mongodb connection error
-    }
-  }
+  });
+
 };
 module.exports = { database };
