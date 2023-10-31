8bc385c0374065ec5e4341ec8baf1c4f1ad7838d
Om Mishra <32200996+mishraomp@users.noreply.github.com>
2023-10-31 16:39:12 -0700

    fix: mark repos soft delete (#276)
    * fix:
    1. mark repo as soft deleted
    2. update chart version
    3. try one values file.
    
    Signed-off-by: OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
    
    * fix:
    1. mark repo as soft deleted
    2. update chart version
    3. try one values file.
    
    Signed-off-by: OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
    
    * fix:
    1. mark repo as soft deleted
    2. update chart version
    3. try one values file.
    
    Signed-off-by: OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
    
    * try different resources.
    
    Signed-off-by: OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
    
    * single values.yaml
    
    Signed-off-by: OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
    
    * adding more logs.
    
    Signed-off-by: OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
    
    * adding more logs.
    
    Signed-off-by: OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
    
    * update to latest version
    
    Signed-off-by: OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
    
    ---------
    
    Signed-off-by: OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>

--- api/src/services/pub-code-service.js
+++ api/src/services/pub-code-service.js
@@ -115,10 +115,26 @@ const health = async (req, res) => {
     res.status(500).json(error);
   }
 };
+const softDeleteRepo = async (req, res) => {
+  try {
+    let pubcodeEntityFromDB = await pubcodeEntity.findOne({ repo_name: req.params.repo_name }).exec();
+    if (!pubcodeEntityFromDB) {
+      res.status(404).json({ message: "Repo Not Found" });
+    } else {
+      await pubcodeEntity.updateOne({ _id: pubcodeEntityFromDB["_id"] }, { is_deleted: true }).exec();
+      pubcodeEntityFromDB = await pubcodeEntity.findOne({ repo_name: req.params.repo_name }).exec();
+      console.info(pubcodeEntityFromDB);
+      res.status(200).json({ message: "Repo Marked as soft deleted." });
+    }
+  } catch (e) {
+    console.error(e);
+  }
 
+};
 module.exports = {
   bulkLoad,
   readAll,
   findById,
-  health
+  health,
+  softDeleteRepo
 };
