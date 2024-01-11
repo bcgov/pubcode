6dcbd21181d300ee2e5028eafb37f3e8a8d34592
Om Mishra <32200996+mishraomp@users.noreply.github.com>
2024-01-11 10:18:09 -0800

    feat: add prometheus (#299)
    Signed-off-by: OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>

--- api/src/app.js
+++ api/src/app.js
@@ -8,9 +8,13 @@ const app = express();
 const apiRouter = express.Router();
 const pubcodeRouter = require("./routes/pubcode-router");
 const log = require("./logger");
+const prom = require('prom-client');
+const register = new prom.Registry();
+prom.collectDefaultMetrics({ register });
+
 const rateLimit = require('express-rate-limit');
 const limiter = rateLimit({
-  windowMs: 1 * 60 * 1000, // 1 minute
+  windowMs: 60 * 1000, // 1 minute
   max: 100, // Limit each IP to 100 requests per `window` (here, per 1 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
@@ -25,7 +29,10 @@ app.use(limiter);
 app.use(cors());
 app.use(helmet());
 app.use(nocache());
-
+app.get('/metrics', async (_req, res) => {
+  const appMetrics = await register.metrics();
+  res.end(appMetrics);
+});
 //tells the app to use json as means of transporting data
 app.use(bodyParser.json({ limit: "50mb", extended: true }));
 app.use(bodyParser.urlencoded({
